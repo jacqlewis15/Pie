3d7a2b0f644d729ac740bf2d1c6f87618d6a25ec
jacqlewis15 <jacqlewis15@gmail.com>
2018-02-25 19:16:34 -0500

    converted run program into TKinter program. UI works

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
+##################################
+## HOW TO SET-UP A RASPBERRY PI ##
+##################################
+
+# 1. Change password from default raspberry:
+#		from command line, input passwd
+# 2. Enable camera and ssh:
+#		preferences -> raspberry pi configuration -> interfaces
+# 3. Install fswebcam: 
+#		sudo apt-get install fswebcam
+#		check that camera works: from command line, input fswebcam "image.jpg"
+# 4. (Optional) to run from startup, edit /etc/rc.local
+
 import RPi.GPIO as GPIO
 import time
 import os
 import subprocess
+from tkinter import *
 
 def writeFile(path, contents):
     with open(path, "wt") as f:
@@ -13,196 +27,165 @@ def readFile(path):
     with open(path, "rt") as f:
         return f.read()
 
-def getPicValue():
-	read = readFile("/home/pi/Documents/values.txt")
-	return int(read.split("\n")[1])
-
-def getPhaseValue():
-	read = readFile("/home/pi/Documents/values.txt")
-	return (read.split("\n")[0] == "on")
-
-def run():
-
-
-	# FIX THE CAMERA! Reset it after each picture?
-
-	# to run from startup, edit /etc/rc.local
-
-	# turns the system on 
-	path = "/home/pi/Documents/values.txt"
-	contents = readFile(path).split("\n")
-	contents[0] = "on"
-	writeFile(path,"\n".join(contents))
-
-	# initializes pins
-	GPIO.setmode(GPIO.BCM)
-	pins = [4,17,27,22,5,6,13,19]
-	lightPins = pins[:2]
-	picPins = pins[2:]
-	GPIO.setup(pins, GPIO.OUT)
-	
-	off = GPIO.HIGH
-	on = GPIO.LOW
-
-	for pin in pins:
-		GPIO.output(pin, off)
-
-	# tests picture folder for validity
-	while(True):
-		picFolder = input("Enter picture storage folder:\n")
-		directory = os.path.dirname("/var/www/html/"+picFolder+"/")
-		if (os.path.isdir(directory)):
-			writeFile("/var/www/html/folder.txt",picFolder)
-			break
-
-	# runs the system
-	while True:
-
-		inWhile = False #included so the pins are all turned off when off
-		phase = getPhaseValue()
-		
-		# initialize the on phase
-		if phase:
-			initTime = time.time()
-			lastPic = initTime
-			picTime = 60*getPicValue()
-			
-			for pin in lightPins:
-				GPIO.output(pin, on)
-			
-			for pin in picPins:
-				GPIO.output(pin, off)
-
-		# only run when in "on" phase
-		while phase:
-
-			inWhile = True
-			
-			#reads from file with multi-access
-			try:
-				phase = getPhaseValue()
-				picTime = 60*getPicValue()
-				currTime = time.time()
-	
-			except:
-				continue
-
-			if (currTime - lastPic) >= picTime:
-				lastPic = currTime
-				date = time.localtime(currTime)
-				picName = time.strftime(":y%ym%md%dH%HM%M.jpg",date)
-				address = "/var/www/html/" + picFolder + "/"
-				# turn lights off
-				for pin in lightPins:
-					GPIO.output(pin, off)
-				
-				alphabet = ["A","B","C","D","E","F"]
-				for i in range(len(picPins)):
-					letter = alphabet[i]
-					# turn pic lights on
-					GPIO.output(picPins[i], on)
-					# take picture
-					subprocess.check_call(["fswebcam",address+picFolder+letter+picName])
-					# turn pic lights off
-					GPIO.output(picPins[i], off)
-
-				# turn lights on
-				for pin in lightPins:
-					GPIO.output(pin, on)
-				
-				
-		if inWhile: 
-			for pin in pins:
-				GPIO.output(pin, off)
-
-
-from tkinter import *
-import os
-
 ####################################
 # UI
 ####################################
 
 def init(data):
-    # load data.xyz as appropriate
-    data.lights = [False]*8
-    data.picTime = "1" # change to 5 later
-    data.edit = [False]*2
-    data.error = ""
-    data.pipe = [False]*2
-    data.time = 0
-    data.folder = "test"
-    data.running = False
-    data.pins = [4,17,27,22,5,6,13,19]
-    data.off = GPIO.HIGH
+	# load data.xyz as appropriate
+	data.lights = [False]*8
+	data.picTime = "1" # change to 5 later
+	data.edit = [False]*2
+	data.error = ""
+	data.pipe = [False]*2
+	data.time = 0
+	data.lastPic = 0
+	data.folder = "test"
+	data.running = False
+	# data.phase = False
+	data.picFolder = data.folder
+	data.newPicTime = int(float(data.picTime)*400)
+
+	GPIO.setmode(GPIO.BCM)
+	data.pins = [4,17,27,22,5,6,13,19]
+	data.lightPins = data.pins[:2]
+	data.picPins = data.pins[2:]
+	GPIO.setup(data.pins, GPIO.OUT)
+	data.off = GPIO.HIGH
 	data.on = GPIO.LOW
 
+	for pin in data.pins:
+		GPIO.output(pin, data.off)
+
 def pressLight(data, light):
-	if data.lights[light]:
-		GPIO.output(data.pins[light],data.off)
-	else:
-		GPIO.output(data.pins[light],data.on)
-    data.lights[light] = not data.lights[light]
-    
+	if not data.running:
+		# if light == 0:
+		# 	GPIO.output(data.pins[0],data.off)
+		if data.lights[light]:
+			if light == 0: 
+				GPIO.output(data.pins[1],data.off)
+				data.lights[1] = False
+			GPIO.output(data.pins[light],data.off)
+			data.lights[light] = False
+		else:
+			for i in range(len(data.pins)):
+				GPIO.output(data.pins[i], data.off)
+				data.lights[i] = False
+			if light == 0: 
+				GPIO.output(data.pins[1],data.on)
+				data.lights[1] = True
+			GPIO.output(data.pins[light],data.on)
+			data.lights[light] = True
+
 
 def press(data, index):
-    if index == 0: pressLight(data, index)
-    if index == 1 and not data.edit[1]: data.edit[0] = True
-    if index == 2 and not data.edit[0]: data.edit[1] = True
-    if index == 3: data.running = not data.running
+	if index == 0 and not data.running: pressLight(data, index)
+	if index == 1 and not data.edit[1]: data.edit[0] = True
+	if index == 2 and not data.edit[0]: data.edit[1] = True
+	if index == 3: 
+		data.running = not data.running
+		if data.running:
+			data.time = 0
+			for i in range(len(data.lightPins)):
+				GPIO.output(data.lightPins[i], data.on)
+				data.lights[i] = True
+			for i in range(len(data.picPins)):
+				GPIO.output(data.picPins[i], data.off)
+				data.lights[i+2] = False
+		else:
+			for i in range(len(data.pins)):
+				GPIO.output(data.pins[i], data.off)
+				data.lights[i] = False
+	if index == 4 and not data.running: takePics(data)
 
 def mousePressed(event, data):
-    # use event.x and event.y
-    margin = 20
-    center = data.width/2
-    bheight = (data.height-margin*5)/8
-    bwidth = data.width/3
-    left = center/2-bwidth/4-margin
-    right = center*3/2-bwidth*3/4+margin
-    index = int((event.y-bheight)//(margin+bheight))
-    top = bheight+index*(margin+bheight)
-    if event.x > left and event.x < (left+bwidth):
-        if event.y > top and event.y < top+bheight: 
-            pressLight(data, index+1)
-    if event.x > right and event.x < (right+bwidth):
-        if event.y > top and event.y < top+bheight: 
-            press(data, index)
+	# use event.x and event.y
+	margin = 20
+	center = data.width/2
+	bheight = (data.height-margin*5)/8
+	bwidth = data.width/3
+	left = center/2-bwidth/4-margin
+	right = center*3/2-bwidth*3/4+margin
+	index = int((event.y-bheight)//(margin+bheight))
+	top = bheight+index*(margin+bheight)
+	if event.x > left and event.x < (left+bwidth):
+		if event.y > top and event.y < top+bheight: 
+			pressLight(data, index+2)
+	if event.x > right and event.x < (right+bwidth):
+		if event.y > top and event.y < top+bheight: 
+			press(data, index)
 
 def isValidFolder(data):
-    return os.path.isdir(data.folder)
+	return os.path.isdir(data.folder)
 
 def keyPressed(event, data):
-    # use event.char and event.keysym
-    if data.edit[0]:
-        if event.keysym == "Return": 
-            if data.picTime.isdecimal(): #add time constraints here
-                data.edit[0] = False
-                data.pipe[0] = False
-            else: data.error = "Must enter valid picture time"
-        elif event.keysym.isdecimal(): 
-            data.error = ""
-            data.picTime += event.keysym
-        elif event.keysym == "BackSpace": 
-            data.picTime = data.picTime[:-1]
-    if data.edit[1]:
-        if event.keysym == "Return":
-            if isValidFolder(data):
-                data.edit[1] = False
-                data.pipe[1] = False
-            else: data.error = "Must enter valid folder name"
-        elif event.keysym == "BackSpace":
-            data.folder = data.folder[:-1]
-        else:
-            data.error = ""
-            data.folder += event.char
+	# use event.char and event.keysym
+	if data.edit[0]:
+		if event.keysym == "Return": 
+			if float(data.picTime) >= 0.5 and float(data.picTime) <= 100.0: #add time constraints here 
+				data.edit[0] = False
+				data.pipe[0] = False
+				data.newPicTime = int(float(data.picTime)*400)
+			else: data.error = "Must enter valid picture time"
+		elif event.keysym.isdecimal(): 
+			data.error = ""
+			data.picTime += event.keysym
+			if len(data.picTime)>7: data.picTime = data.picTime[:-1]
+		elif event.keysym == "BackSpace": 
+			data.picTime = data.picTime[:-1]
+		elif event.keysym == "period" and (data.picTime.isdecimal() or data.picTime == ""):
+			data.picTime += "."
+	if data.edit[1]:
+		if event.keysym == "Return":
+			if isValidFolder(data):
+				data.edit[1] = False
+				data.pipe[1] = False
+				data.picFolder = data.folder
+			else: data.error = "Must enter valid folder name"
+		elif event.keysym == "BackSpace":
+			data.folder = data.folder[:-1]
+		else:
+			data.error = ""
+			data.folder += event.char
+		if len(data.folder) > 18: data.folder = data.folder[:-1]
+
+def takePics(data):
+
+	date = time.localtime(time.time())
+	picName = time.strftime(":y%ym%md%dH%HM%M.jpg",date)
+	address = data.picFolder + "/"
+	
+	for i in range(len(data.pins)):
+		GPIO.output(data.pins[i], data.off)
+		data.lights[i] = False
+
+	alphabet = ["A","B","C","D","E","F"]
+	for i in range(len(data.picPins)):
+		letter = alphabet[i]
+		# turn pic lights on
+		GPIO.output(data.picPins[i], data.on)
+		# take picture
+		subprocess.check_call(["fswebcam",address+data.picFolder+letter+picName,"-S 1"])
+		# time.sleep(1)
+		# turn pic lights off
+		GPIO.output(data.picPins[i], data.off)
 
 def timerFired(data):
-    if data.edit[0]:
-        data.time += 1
-        if data.time % 5 == 0: data.pipe[0] = not data.pipe[0]
-    if data.edit[1]:
-        data.time += 1
-        if data.time % 5 == 0: data.pipe[1] = not data.pipe[1]
+	if data.running:
+		if (data.time - data.lastPic) >= data.newPicTime:
+			data.lastPic = data.time
+			takePics(data)
+			# turn lights on
+			for i in range(len(data.lightPins)):
+				GPIO.output(data.lightPins[i], data.on)
+				data.lights[i] = True
+	if data.edit[0]:
+		# data.time += 1
+		if data.time % 5 == 0: data.pipe[0] = not data.pipe[0]
+	if data.edit[1]:
+		# data.time += 1
+		if data.time % 5 == 0: data.pipe[1] = not data.pipe[1]
+	data.time += 1
 
 def isOn(data, light):
     if data.lights[light]: return "Off"
@@ -241,7 +224,9 @@ def drawButtons(canvas, data):
             else: text2 = "Start run"
             font2 = "Arial 20 bold"
         if i == 4:
-            text2 = ""
+            text2 = "Take Pictures"
+        if i == 5:
+        	text2 = ""
         canvas.create_text(right+bwidth/2,top+bheight/2,text=text2,font=font2)
 
 def redrawAll(canvas, data):
@@ -294,9 +279,10 @@ def runUI(width=300, height=300):
     timerFiredWrapper(canvas, data)
     # and launch the app
     root.mainloop()  # blocks until window is closed
+    GPIO.cleanup()
 
 runUI(800, 600)
-# try: run()
+# try: runUI(800,600)
 # except:
 # 	print("oops!")
 # 	GPIO.cleanup()
